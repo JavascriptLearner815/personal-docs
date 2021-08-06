583329f1205521a7f8cd786621cb4d53b213143e
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-08-05 22:13:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.getElementById("print").addEventListener("click", () => {
 })
 
 document.getElementById("clear").addEventListener("click", () => {
-  if (confirm("Are you sure you wish to clear your document? After it is cleared, you CANNOT ever get its current contents back. Are you still certain? (Cancel to Cancel, Ok to continue.)")) {
-    if (confirm("Are you really, 100% sure? This is your last warning. If you choose Ok the current contents of this document will be lost forever. Press Cancel and you'll be safe.")) {
-      document.getElementById("editor").innerHTML = ""
+  if (confirm("Are you sure you wish to clear your document? After it is cleared, you CANNOT ever get its current contents back. Are you still certain? (Cancel to Cancel, OK to continue.)")) {
+    if (confirm("Are you really, 100% sure? This is your last warning. If you choose OK the current contents of this document will be lost forever. Press Cancel and you'll be safe.")) {
+      document.getElementById("editor").innerHTML = "<p>Use the toolbar above to<strong>edit the text format or clear it.</strong></p>"
     }
   }
 })
