d4ebf8366d92e34ef515d25fd4e3826140c5ecb8
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-08-05 22:21:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById("print").addEventListener("click", () => {
 })
 
 document.getElementById("clear").addEventListener("click", () => {
-  if (confirm("Are you sure you wish to clear your document? After it is cleared, you CANNOT ever get its current contents back. Are you still certain? (Cancel to Cancel, OK to continue.)")) {
-    if (confirm("Are you really, 100% sure? This is your last warning. If you choose OK the current contents of this document will be lost forever. Press Cancel and you'll be safe.")) {
+  if (confirm("***********IMPORTANT!***********\nAre you sure you really wish to clear your document? After it is cleared, you CANNOT ever get its current contents back. Are you still certain? (Cancel to Cancel, OK to continue.)")) {
+    if (confirm("***********IMPORTANT!***********\nAre you really, 100% sure? This is your last warning. If you choose OK the current contents of this document will be lost forever. Press Cancel and you'll be safe.")) {
       document.getElementById("editor").innerHTML = "<p>Use the toolbar above to<strong>edit the text format or clear it.</strong></p>"
       refresh()
     }
